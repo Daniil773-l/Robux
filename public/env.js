a635f354e8c0e508407171c3372f8ce8e7e9dc4f
pikoUsername <galymzhan.amantaj@gmail.com>
2024-09-22 20:09:27 +0500

    Ok

--- public/env.js
+++ public/env.js
@@ -1,3 +1,3 @@
 window.env = {
-  "BACKEND_HOST": "http://localhost:8000"
+  "BACKEND_HOST": ""
 };
--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
     return (
         <div>
             <AppContainer>
-                <DesktopHeader/>
+                <Stats/>
                 <Buy />
                 <Cards />
                 <GiftCards />
